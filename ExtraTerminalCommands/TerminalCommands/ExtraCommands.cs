5243b0743e6624441550c65a6009fb174f45454a
Zachary Haber <zjhaber@me.com>
2024-05-12 12:46:50 -0500

    Allow Terminal API to show all the available commands for the Extra category

--- ExtraTerminalCommands/TerminalCommands/ExtraCommands.cs
+++ ExtraTerminalCommands/TerminalCommands/ExtraCommands.cs
@@ -13,39 +13,10 @@ namespace ExtraTerminalCommands.TerminalCommands
             CommandInfo cmdInfo = new CommandInfo
             {
                 Category = "other",
-                Description = "Shows every command from the \"ExtraCommands\" mod.",
-                DisplayTextSupplier = onExtraCommands
+                Description = "Shows extra commands.",
             };
-            AddCommand("EXTRA", cmdInfo);
-            AddCommand("Extra", new CommandInfo { Category = "none", Description = "Inverse teleports everyone on the inverse teleporter.", DisplayTextSupplier = onExtraCommands });
-            AddCommand("EC", new CommandInfo { Category = "none", Description = "Inverse teleports everyone on the inverse teleporter.", DisplayTextSupplier = onExtraCommands });
-            AddCommand("Extra-Commands", new CommandInfo { Category = "none", Description = "Inverse teleports everyone on the inverse teleporter.", DisplayTextSupplier = onExtraCommands });
-            AddCommand("Extra Commands", new CommandInfo { Category = "none", Description = "Inverse teleports everyone on the inverse teleporter.", DisplayTextSupplier = onExtraCommands });
+            AddCommand("extra", cmdInfo);
         }
 
-        private static string onExtraCommands()
-        {
-            ETCNetworkHandler NH = ETCNetworkHandler.Instance;
-            if (NH.extraCmdDisabled)
-            {
-                return "This command is disabled by the host.\n";
-            }
-
-            String message = "Extra Terminal Commands:";
-
-            if (!NH.tpCmdDisabled) { message += "\n\n>TP\n" + TeleportCommand.description; }
-            if (!NH.itpCmdDisabled) { message += "\n\n>ITP\n" + InverseTeleportCommand.description; }
-            if (!NH.randomCmdDisabled) { message += "\n\n>RANDOM [WEATHER]\n" + RandomMoonCommand.description; }
-            if (!NH.launchCmdDisabled) { message += "\n\n>LAUNCH\n" + LaunchCommand.description; }
-            if (!NH.hornCmdDisabled) { message += "\n\n>HORN [SECONDS]\n" + HornCommand.description; }
-            if (!NH.doorCmdDisabled) { message += "\n\n>DOORS\n" + DoorsCommand.description; }
-            if (!NH.lightCmdDisabled) { message += "\n\n>LIGHTS\n" + LightsCommand.description; }
-            if (!NH.introCmdDisabled) { message += "\n\n>INTRO\n" + IntroSongCommand.description; }
-            if (!NH.timeCmdDisabled) { message += "\n\n>TIME\n" + TimeCommand.description; }
-            if (!NH.clearCmdDisabled) { message += "\n\n>CLEAR\n" + ClearScreenCommand.description; }
-            if (!NH.switchCmdDisabled) { message += "\n\n>S\n" + SwitchCommand.description; }
-
-            return message + "\n\n";
-        }
     }
 }
